d6da850f2300db104c499db45340a85f64c88caf
Сергей <gan7er@yandex.ru>
2021-10-25 20:25:53 +0300

    enum

--- LessonOOP-2/LessonOOP-2/Program.cs
+++ LessonOOP-2/LessonOOP-2/Program.cs
@@ -1,11 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LessonOOP_2
 {
+    public enum AccauntType {};
+    public class BankAccount
+    {
+       private int Nomber;
+       private decimal Balance;
+       public AccauntType type;
+        public void Rec(int a,decimal b)
+        {
+            this.Nomber = a;
+            this.Balance = b;
+           
+       }
+        public void Read()
+        {
+            Console.WriteLine($"Номер счета - {Nomber}\r\nБаланс - {Balance}");
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+           
+            BankAccount bank = new BankAccount();
+            bank.Rec(11111, 6989);
+            bank.Read();
+            Console.WriteLine(bank.type);
+            bank.type
             
         }
     }
