b31a6b5b5746cd842ae4466c27b231dbe82c4337
Сергей <gan7er@yandex.ru>
2021-10-27 21:00:13 +0300

    valid variant

--- LessonOOP-2/LessonOOP-2/Program.cs
+++ LessonOOP-2/LessonOOP-2/Program.cs
@@ -6,54 +6,25 @@ namespace LessonOOP_2
     public enum AccauntType {Credit,Debit}
     public class BankAccount
     {
-        
         private decimal Balance;
         private AccauntType Type;
         private static int UnicNomber;
 
         public BankAccount(decimal b, AccauntType c)
         {
-           
+            UnicNomber++;
             this.Balance=b;
             this.Type = c;
-            UnicNomber = UnicNomber++;
+            
+            Console.WriteLine($"Номер счета - {UnicNomber}\r\nБаланс - {Balance}\r\nТип счета - {Type}");
         }
-        
-        //public void Rec(decimal b,AccauntType c)
-        //{
-        //    UnicNomber = GetUnicnomber();
-        //    this.Balance = b;
-        //    this.Type = c;
-        //}
-        //public void Read()
-        //{
-        //    Console.WriteLine($"Номер счета - {UnicNomber}\r\nБаланс - {Balance}\r\nТип счета - {Type}");
-        //}
-        //public int GetUnicnomber()
-        //{
-        //        UnicNomber++;
-        //        return UnicNomber;
-        //}
-
-    }
+     }
     class Program
     {
         static void Main(string[] args)
         {
-
-            //BankAccount bank = new BankAccount();
-            //bank.Rec(565655, AccauntType.Credit);
-            //bank.Read();
-            //BankAccount bank1 = new BankAccount();
-            //bank1.Rec(5555, AccauntType.Debit);
-            //bank1.Read();
-            //BankAccount bank2 = new BankAccount();
-            //bank2.Rec(66666, AccauntType.Credit);
-            //bank2.Read();
-            decimal c = 56;
-            BankAccount bank = new { 555, AccauntType.Credit };
-
-
+            BankAccount bank = new BankAccount(55555,AccauntType.Credit);
+            BankAccount bank1 = new BankAccount(66666, AccauntType.Credit);
         }
     }
 }
