d0d41fcdfd418d9fdf320e76d5e508d6459e7b2f
Сергей <gan7er@yandex.ru>
2021-10-28 19:47:53 +0300

    add balance invalid

--- LessonOOP-2/LessonOOP-2/Program.cs
+++ LessonOOP-2/LessonOOP-2/Program.cs
@@ -22,6 +22,10 @@ namespace LessonOOP_2
                 unicNomber++;
             }
         }
+        public void GetBalance(BankAccount unicnomber,decimal summ)
+        {
+           Balance = Balance + summ;
+        }
         
     }
     class Program
@@ -30,15 +34,27 @@ namespace LessonOOP_2
         {
 
             BankAccount bank = new BankAccount();
-            bank.Balance = 5555;
+            bank.Balance = 4444;
             bank.Type = AccauntType.Credit;
             bank.UnicNomber = 0;
             Console.WriteLine($"Номер счета - {bank.UnicNomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
+            
+            var a = bank;
+           // var b = bank1;
+            bank.GetBalance(a, 1111);
+            Console.WriteLine($"Номер счета - {bank.UnicNomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
+            bank.GetBalance(a, 1111);
+            Console.WriteLine($"Номер счета - {bank.UnicNomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
             BankAccount bank1 = new BankAccount();
-            bank1.Balance = 6666;
+            bank1.Balance = 2222;
             bank1.Type = AccauntType.Debit;
             bank1.UnicNomber = 0;
-            Console.WriteLine($"Номер счета - {bank1.UnicNomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
+            BankAccount bank2 = new BankAccount();
+            bank2.Balance = 1111;
+            bank2.Type = AccauntType.Debit;
+            bank2.UnicNomber = 0;
+            bank2.GetBalance(a, 2222);
+            Console.WriteLine($"Номер счета - {bank2.UnicNomber}\r\nБаланс - {bank2.Balance}\r\nТип счета - {bank2.Type}");
 
         }
     }
