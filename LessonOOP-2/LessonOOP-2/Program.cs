3e0500dc96b72ca46d8886fa850991a7d60ed589
Сергей <gan7er@yandex.ru>
2021-10-29 19:50:29 +0300

    invalid unic

--- LessonOOP-2/LessonOOP-2/Program.cs
+++ LessonOOP-2/LessonOOP-2/Program.cs
@@ -27,6 +27,7 @@ namespace LessonOOP_2
            
            this.Balance = Balance + summ;
            unicnomber.Balance =unicnomber.Balance-summ;
+            
         }
         
     }
@@ -45,6 +46,7 @@ namespace LessonOOP_2
             bank1.Type = AccauntType.Debit;
             bank1.UnicNomber = 0;
             Console.WriteLine($"Номер счета - {bank1.UnicNomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
+
             bank.GetBalance(bank1, 1111);
             Console.WriteLine($"Номер счета - {bank.UnicNomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
             Console.WriteLine($"Номер счета - {bank1.UnicNomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
