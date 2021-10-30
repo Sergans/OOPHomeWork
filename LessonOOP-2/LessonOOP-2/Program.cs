6cdb55a4bad7efa9478924eafcf6b1587eb2ddcf
Сергей <gan7er@yandex.ru>
2021-10-30 16:05:09 +0300

    Lesson-3-1

--- LessonOOP-2/LessonOOP-2/Program.cs
+++ LessonOOP-2/LessonOOP-2/Program.cs
@@ -2,54 +2,56 @@
 using System.Collections.Generic;
 
 namespace LessonOOP_2
-{
+{// задание 3-1
     public enum AccauntType {Credit,Debit}
     public class BankAccount
     {
-        
-        public decimal Balance { get; set; }
-        public AccauntType Type { get; set; }
-        private static int unicNomber;
-        public int UnicNomber
+
+        private decimal balance;
+        public decimal Balance
         {
             get
             {
-                return unicNomber;
+                return balance;
             }
-            set
-            {
-
-                unicNomber++;
-            }
-        }
-        public void GetBalance(BankAccount unicnomber,decimal summ)
-        {
            
-           this.Balance = Balance + summ;
-           unicnomber.Balance =unicnomber.Balance-summ;
-            
         }
+        public AccauntType Type { get; }
+        private static int unicNomber;
+        public int Nomber { get; }
         
+        public BankAccount(decimal balance,AccauntType type)
+        {
+            this.balance = balance;
+            this.Type = type;
+            Nomber= unicNomber++;
+        }
+        public void GetBalance(BankAccount accaunt, decimal summ)
+        {
+
+            this.balance = this.balance + summ;
+            accaunt.balance = accaunt.balance - summ;
+
+        }
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-
-            BankAccount bank = new BankAccount();
-            bank.Balance = 4444;
-            bank.Type = AccauntType.Credit;
-            bank.UnicNomber = 0;
-            Console.WriteLine($"Номер счета - {bank.UnicNomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
-            BankAccount bank1 = new BankAccount();
-            bank1.Balance = 3333;
-            bank1.Type = AccauntType.Debit;
-            bank1.UnicNomber = 0;
-            Console.WriteLine($"Номер счета - {bank1.UnicNomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
-
+            BankAccount bank = new BankAccount(4444, AccauntType.Credit);
+            BankAccount bank1 = new BankAccount(3333, AccauntType.Debit);
+            BankAccount bank2 = new BankAccount(2222, AccauntType.Credit);
+            // проверка
+            Console.WriteLine($"Номер счета - {bank.Nomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
+            Console.WriteLine($"Номер счета - {bank1.Nomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
+            Console.WriteLine($"Номер счета - {bank2.Nomber}\r\nБаланс - {bank2.Balance}\r\nТип счета - {bank2.Type}");
             bank.GetBalance(bank1, 1111);
-            Console.WriteLine($"Номер счета - {bank.UnicNomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
-            Console.WriteLine($"Номер счета - {bank1.UnicNomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
+            Console.WriteLine($"Номер счета - {bank.Nomber}\r\nБаланс - {bank.Balance}\r\nТип счета - {bank.Type}");
+            Console.WriteLine($"Номер счета - {bank1.Nomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
+            bank2.GetBalance(bank1, 2222);
+            Console.WriteLine($"Номер счета - {bank1.Nomber}\r\nБаланс - {bank1.Balance}\r\nТип счета - {bank1.Type}");
+            Console.WriteLine($"Номер счета - {bank2.Nomber}\r\nБаланс - {bank2.Balance}\r\nТип счета - {bank2.Type}");
         }
     }
 }
