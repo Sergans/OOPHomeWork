01f1a9736848a6bf5574c4292b6b70cd3acd678f
Сергей <gan7er@yandex.ru>
2021-11-05 12:21:48 +0300

    string fraction

--- Lesson-5-1/Lesson-5-1/Program.cs
+++ Lesson-5-1/Lesson-5-1/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Lesson_5_1
 {
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        public Fraction(int _numerator,int _denomirator)
+        {
+            Numerator = _numerator;
+            Denominator = _denomirator;
+        }
+        public override string ToString()
+         {
+            return ($"{this.Numerator}\r\n-\r\n{this.Denominator}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Fraction n1 = new Fraction(1, 2);
+            Console.WriteLine(n1.ToString());
         }
     }
 }
