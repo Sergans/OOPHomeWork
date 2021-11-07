a84854e5c1cbd2d8bff53c75b09716ce8dcc223d
Сергей <gan7er@yandex.ru>
2021-11-07 12:09:25 +0300

    add summ

--- Lesson-5-1/Lesson-5-1/Program.cs
+++ Lesson-5-1/Lesson-5-1/Program.cs
@@ -11,8 +11,23 @@ namespace Lesson_5_1
             Numerator = _numerator;
             Denominator = _denomirator;
         }
+        public static Fraction operator +(Fraction f1,Fraction f2)
+        {
+            if (f1.Denominator == f2.Denominator)
+            {
+                return new Fraction(f1.Numerator + f2.Numerator, f1.Denominator);
+            }
+            
+            int numer1 = f1.Numerator * f2.Denominator;
+            int denom1 = f1.Denominator * f2.Denominator;
+            int numer2 = f2.Numerator * f1.Denominator;
+            int denom2 = f2.Denominator * f1.Denominator;
+            Fraction _f1 =new Fraction(numer1,denom1);
+            Fraction _f2=new Fraction(numer2,denom2);
+            return new Fraction(_f1.Numerator + _f2.Numerator, _f1.Denominator);
+        }
         public override string ToString()
-         {
+        {
             return ($"{this.Numerator}\r\n-\r\n{this.Denominator}");
         }
     }
@@ -20,8 +35,10 @@ namespace Lesson_5_1
     {
         static void Main(string[] args)
         {
-            Fraction n1 = new Fraction(1, 2);
-            Console.WriteLine(n1.ToString());
+            Fraction n1 = new Fraction(1, 3);
+            Fraction n2 = new Fraction(2, 3);
+            var a = n1 + n2;
+            Console.WriteLine(a.ToString());
         }
     }
 }
